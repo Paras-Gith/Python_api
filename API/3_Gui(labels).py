620c1420076919fc535e412ddad32690c09fb4f2
Paras-Gith <161969750+Paras-Gith@users.noreply.github.com>
2026-02-12 16:59:48 +0530

    Add files via upload

---
+++ API/3_Gui(labels).py
@@ -0,0 +1,33 @@
+#pyqt5 labels
+import sys 
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+
+        label = QLabel("Hello", self)
+        label.setFont(QFont("Arial", 40))
+        label.setGeometry(0, 0, 500, 100)
+        label.setStyleSheet("color: blue;"
+                            "background-color: red;"
+                            "font-weight: bold;"
+                            "font-style: italic;"
+                            "text-decoration: underline;")
+        # label.setAlignment(Qt.AlignVCenter)
+        # label.setAlignment(Qt.AlignHCenter)
+        label.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
